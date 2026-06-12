baa5836dde11d82ec3560d116ff7b4d6db5b3667
m-ryu-droid <m-ryu@co-success.jp>
2026-06-12 11:48:23 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   googleClientId: '98419556061-l2kh0kas1cg3nul8jmdl8tns8c195c2b.apps.googleusercontent.com',
   allowedSheetName: 'mail_recipients',
-  allowedDomains: [co-success.jp],
+  allowedDomains: ['co-success.jp'],
   allowedEmails: [],
 };
